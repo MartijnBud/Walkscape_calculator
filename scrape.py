06c52d61c29636a6f481f2b13585adff0a0b360f
Sander Buddendorf <swjbuddendorf@gmail.com>
2024-09-17 21:34:55 +0200

    wrangling dfs

--- scrape.py
+++ scrape.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
-
+""" Scrape tables from wiki page """
 def get_tables(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
@@ -16,8 +17,9 @@ def get_tables(url):
         'activities': [],
         'other': []}
     for table in tables:
+
         if not table.find('tr'):
-            # Skip tables with no rows
+            # Skip empty tables
             continue
 
         if "This page has been updated to reflect" in table.find("tr").text:
@@ -45,31 +47,57 @@ def get_tables(url):
 
     return labeled_dfs
 
+""" Function to make csv with all activities and recipes, scraped from walkscape wiki """
+def get_activites():
+    # Get list of Skills
+    dfs = get_tables("https://wiki.walkscape.app/wiki/Skills")
+    skills = dfs["other"][0]["Skill.1"]
 
-# Use Skills table from Skills page to extract skills
-dfs = get_tables("https://wiki.walkscape.app/wiki/Skills")
-skills = dfs["other"][0]["Skill.1"]
+    # Get df with all recipes and all activities
+    path = "https://wiki.walkscape.app/wiki/"
 
-# Get table with activities per skill
-path = "https://wiki.walkscape.app/wiki/"
+    combined_dfs = {
+        'recipes': [],
+        'activities': [],
+        'other': []
+    }
 
-combined_dfs = {
-    'recipes': [],
-    'activities': [],
-    'other': []
-}
+    for skill in skills:
+        print(f"Getting activity of {skill}")
+        path_skill = path + skill
+        dfs = get_tables(path_skill)
 
-for skill in skills:
-    print(f"Getting activity of {skill}")
-    path_skill = path + skill
-    dfs = get_tables(path_skill)
+        for df in dfs["recipes"]:
+            print(df)
+            df["skill"] = skill
 
-    # Merge the results into the combined_dfs dictionary
-    for label in combined_dfs:
-        combined_dfs[label].extend(dfs.get(label, []))
 
-df_recipes = pd.concat(combined_dfs["recipes"])
-df_activities = pd.concat(combined_dfs["activities"])
+        # Merge the results into the combined_dfs dictionary
+        for label in combined_dfs:
+            combined_dfs[label].extend(dfs.get(label, []))
 
-df_recipes.to_csv("data/recipes.csv")
-df_activities.to_csv("data/activities.csv")
+    df_recipes = pd.concat(combined_dfs["recipes"])
+    df_activities = pd.concat(combined_dfs["activities"])
+
+    # Clean recipes and activities before merging
+    df_activities.rename(columns=str.lower)
+    df_recipes.rename(columns=str.lower)
+
+    df_activities.rename(columns={"level(s)": "level", "location": "location(s)", "activity.1": "activity",
+                                  "skill(s)": "skill", "requirement(s)": "requirement"})
+    df_recipes.rename(columns={"activity.1": "activity", "service requirement": "location",
+                               "materials used": "requirement"})
+
+    df_activities['Min Steps (Max Efficiency)'] = df_activities['Min Steps (Max Efficiency)'].str.replace(r'\s*\(.*?\)', '', regex=True)
+    df_activities.columns = [re.sub(r'\s*\(.*?\)', '', col) for col in df_activities.columns]
+    df_recipes['Min Steps (Max Efficiency)'] = df_recipes['Min Steps (Max Efficiency)'].str.replace(r'\s*\(.*?\)', '', regex=True)
+    df_recipes.columns = [re.sub(r'\s*\(.*?\)', '', col) for col in df_recipes.columns]
+
+    df_activities = df_activities.drop(df_activities.columns[:2], axis=1)
+    df_recipes = df_recipes.drop(df_recipes.columns[:2], axis=1)
+
+
+    df_recipes.to_csv("data/recipes.csv")
+    df_activities.to_csv("data/activities.csv")
+
+get_activites()
